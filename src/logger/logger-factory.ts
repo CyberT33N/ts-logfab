7a58330478b54e70b8d675ac6cf4bf8b8903da90
CyberT33N <checkGitHubForEmail@gmail.com>
2025-07-12 18:01:34 +0200

    fixed linting in pino prettifier

--- src/logger/logger-factory.ts
+++ src/logger/logger-factory.ts
@@ -19,7 +19,7 @@ import { join } from 'path'
 import { pino } from 'pino'
 import pretty from 'pino-pretty'
 import { PackageJson } from 'zod-package-json'
-import { createEnterprisePrettyConfig } from '@/logger/pino-prettifiers.ts'
+import { createEnterprisePrettyConfig } from '@/prettifiers/pino-prettifiers.ts'
 
 /**
  * 🎯 Creates an enterprise logger instance
