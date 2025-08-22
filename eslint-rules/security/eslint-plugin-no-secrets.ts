3cf13746c0259817cec4309108317bc89bf0896f
CyberT33N <checkGitHubForEmail@gmail.com>
2025-08-23 00:08:58 +0200

    fixed linting in eslint-plugin-no-secrets.ts

--- eslint-rules/security/eslint-plugin-no-secrets.ts
+++ eslint-rules/security/eslint-plugin-no-secrets.ts
@@ -157,15 +157,13 @@ const noSecretsRules: {
  * Creates the no-secrets plugin configuration.
  * @returns The no-secrets plugin configuration.
  */
-const createNoSecretsConfig = (): TSESLint.FlatConfig.Config => {
-    return {
-        name: 'enterprise/security/no-secrets',
-        plugins: {
-            'no-secrets': noSecrets
-        },
-        rules: noSecretsRules.rules
-    }
-}
+const createNoSecretsConfig = (): TSESLint.FlatConfig.Config => ({
+    name: 'enterprise/security/no-secrets',
+    plugins: {
+        'no-secrets': noSecrets
+    },
+    rules: noSecretsRules.rules
+})
 
 /**
  * Creates the all security ESLint rules.
